c386bfd56434d292a44a9db3b2c307b1a18791eb
tapetone <151849237+tapetone@users.noreply.github.com>
2024-01-10 00:21:02 +0300

    navbar update

--- app/layout.js
+++ app/layout.js
@@ -12,10 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-      </body>
+      <Navbar />
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
