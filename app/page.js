e2d1d841026c40f7221c7a049c9654110fbffff0
Ilona <lona.krechko@gmail.com>
2024-01-10 23:52:07 +0000

    Added search post by hashtag and added logic to the addPostCard

--- app/page.js
+++ app/page.js
@@ -8,15 +8,17 @@ export default function Home() {
       username: "Alex",
       text: "Cats, known for their graceful agility and independent demeanor, have captivated humans for centuries with their mysterious charm. From their soothing purrs to playful antics, these enigmatic feline companions continue to be beloved members of households worldwide.",
       image: "http://placekitten.com/500/500",
-      likes: 0,
-    },
-    {
-      username: "A",
-      text: "Cats, known for their graceful agility and independent demeanor, have captivated humans for centuries with their mysterious charm. From their soothing purrs to playful antics, these enigmatic feline companions continue to be beloved members of households worldwide.",
-      image: "http://placekitten.com/500/500",
+      hashtags: ["#cats", "#lovacats"],
       likes: 0,
     },
   ]);
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchValue = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   /**
    * Posts is an array of objects. Each post is object with following fields:
    * image
@@ -40,10 +42,30 @@ export default function Home() {
     setPosts(posts);
   }, []);
 
-  
+  let postsTorender = posts;
+
+  if (searchValue !== "") {
+    const filteredPosts = posts.filter((post) => {
+      return post.hashtags.includes(searchValue);
+    });
+    postsTorender = filteredPosts;
+  }
+
   return (
     <main className="flex flex-col gap-20 justify-center w-screen py-20 items-center">
-      {posts.map((post, index) => {
+      <div className="flex flex-col items-center gap-5 w-full">
+        <label htmlFor="search" className="block text-xl font-medium leading-6">
+          Search post
+        </label>
+        <input
+          onChange={handleSearchValue}
+          type="text"
+          name="search"
+          className="block w-1/2 rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-700 sm:text-sm sm:leading-6"
+        />
+      </div>
+
+      {postsTorender.map((post, index) => {
         return (
           <MediaCard
             post={post}
--- components/addPostCard.jsx
+++ components/addPostCard.jsx
@@ -33,21 +33,24 @@ const AddPostCard = () => {
         }
 
         // Getting the data from the local storage
-        const existingPosts = JSON.parse(localStorage.getItem('posts')); // Posts is the name of the key in the local storage. So we can access
+        //Ilona - Changed const to let because we are reasigning it to empty []
+        let existingPosts = JSON.parse(localStorage.getItem('posts')); // Posts is the name of the key in the local storage. So we can access
 
+        //Ilona - changed return to existingPosts = [] to handle empty local storage case
         // Error Handling: If there is no 'posts' in local storage, nothing will happen
         if (!existingPosts) {
-            return;
+            existingPosts = [];
         }
         
         // Resets the PostObj, with the updated data (Line 20)
-        setPostObj(() => { 
-            // Returns the back to postObj + with the Likes Key and 0 Value
-            return {...postObj, likes: 0}
-        });
+        //Ilona - merged this logic into the posts for optimization
+        // setPostObj(() => { 
+        //     // Returns the back to postObj + with the Likes Key and 0 Value
+        //     return {...postObj, likes: 0}
+        // });
 
         // Concats the existing post with the new post
-        const posts = [...existingPosts, postObj];
+        const posts = [...existingPosts, {...postObj, likes: 0, hashtags: postObj.hashtags.split(" ")}];
 
         // Update Local storage with the existing and new post
         localStorage.setItem('posts', JSON.stringify(posts));
@@ -60,12 +63,12 @@ const AddPostCard = () => {
                 <div className=" w-full h-24 flex">
                     <div  className="flex items-center justify-center w-1/6">
                     <div className="rounded-full border-2 border-black h-16 w-16 flex items-center justify-center">
-                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 18">
+                        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 14 18">
                             <path d="M7 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9Zm2 1H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
                         </svg>
                     </div>  
                     </div>
-                    <input onChange={handleInput} class='w-3/4 h-23 border-black border-2 pl-2 hover:border-sky-700 hover:border-4 rounded-md' placeholder='Enter your username' name="username"/>
+                    <input onChange={handleInput} className='w-3/4 h-23 border-black border-2 pl-2 hover:border-sky-700 hover:border-4 rounded-md' placeholder='Enter your username' name="username"/>
                 </div>
 
                 {/* Text */}
@@ -73,7 +76,7 @@ const AddPostCard = () => {
                     <div className="flex items-center justify-center w-1/6">
                         <h2 className='font-bold'>Text: </h2>
                     </div>
-                    <input onChange={handleInput} class='w-3/4 h-23 border-black border-2 pl-2 hover:border-sky-700 hover:border-4 rounded-md'  type="textarea" placeholder='Add your message' name="text" />
+                    <input onChange={handleInput} className='w-3/4 h-23 border-black border-2 pl-2 hover:border-sky-700 hover:border-4 rounded-md'  type="textarea" placeholder='Add your message' name="text" />
                 </div>
 
                 {/* Images */}
@@ -81,13 +84,23 @@ const AddPostCard = () => {
                     <div className="flex items-center justify-center w-1/6">
                         <h2 className='font-bold'>Link: </h2>
                     </div>
-                    <input onChange={handleInput} class='w-3/4 h-23 border-black border-2 pl-2 hover:border-sky-700 hover:border-4' placeholder='Add Image Link' name="images" />
+                    <input onChange={handleInput} className='w-3/4 h-23 border-black border-2 pl-2 hover:border-sky-700 hover:border-4' placeholder='Add Image Link' name="images" />
+                </div>
+
+                {/* Hashtags */}
+                <div className="w-full h-24 flex">
+                    <div className="flex items-center justify-center w-1/6">
+                        <h2 className='font-bold'>Hashtags: </h2>
+                    </div>
+                    <input onChange={handleInput} className='w-3/4 h-23 border-black border-2 pl-2 hover:border-sky-700 hover:border-4' placeholder='Add hashtags' name="hashtags" />
                 </div>
 
                 {/* Submit Button */}
-                <div class='w-full h-24 relative'>
+                <div className='w-full h-24 relative'>
                     <button type='submit' className='absolute right-10 top-5 border-black border-2 h-16 w-28 rounded-lg hover:bg-black hover:text-white'>Post</button>
                 </div>
+
+
             </form>
         </div>
     )
