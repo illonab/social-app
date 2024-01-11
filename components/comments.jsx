df2e24e22eb5cfec9201a9508ac129552bd3c0b6
Tony K. <tony-koke@outlook.com>
2024-01-11 15:56:35 +0000

    Added Local Storage

--- components/comments.jsx
+++ components/comments.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Comment = ({ postIndex, comments, onUpdatePost }) => {
   const [newComment, setNewComment] = useState('');
-
   const onAddComment = () => {
     if (newComment.trim() !== '') {
       const updatedComments = [...comments, newComment];
@@ -10,12 +9,20 @@ const Comment = ({ postIndex, comments, onUpdatePost }) => {
       setNewComment('');
     }
   };
-
+    // Load comments from local storage on component
+    useEffect(() => {
+        const storedComments = localStorage.getItem('comments');
+        if (storedComments) {
+          setNewComment (JSON.parse(storedComments));
+        }
+      }, []);
+      // Save comments to local storage whenever comments change
+      useEffect(() => {
+        localStorage.setItem('comments', JSON.stringify(comments));
+      }, [comments]);
   return (
     <div>
-      
     </div>
   );
 };
-
 export default Comment;
